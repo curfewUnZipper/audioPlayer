50e6a904ab73c1920c4100d49d2170c7b3f5ae82
curfewUnzipper <cryingpushpa@gmail.com>
2023-11-18 13:06:42 +0530

    modified:   server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 const express = require('express')
 const app= express()
-// app.set('view engine','ejs') //anpother engine: pug
+app.set('view engine','ejs') //anpother engine: pug
 app.use(express.urlencoded({extended:true})) //middleware to excess form values
 
 //bootstrap + jquery
